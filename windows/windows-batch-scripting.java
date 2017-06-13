185eec10ae5785259d2ed75cbd61bca144a739b9
anhvurz90 <anhvurz90@gmail.com>
2017-06-13 10:45:39 +0700

    Add WindowsBatchScripting-01: Getting Started

---
+++ windows/windows-batch-scripting.java
@@ -0,0 +1,32 @@
+	WINDOWS BATCH SCRIPTING:
+Link: http://steve-jansen.github.io/guides/windows-batch-scripting/part-1-getting-started.html
+0.Overview
+1.Getting Started: {
+  1.1.Getting Started with Windows Batch Scripting:
+  1.2.Launching the Command Prompt: {
+    - Windows + R > cmd
+  }
+  1.3.Editting Batch Files
+  1.4.Viewing Batch Files: {
+    - Nodepad
+    - Type myscript.cmd
+    - More myscript.cmd
+    - Edit myscript.cmd
+  }
+  1.5.Batch File Names and File Extensions {
+    - .cmd is better than .bat (due to some rare side effects with .bat files)
+  }
+  1.6.Saving Batch Files in Windows
+  1.7.Running your Batch File:
+  1.8.Comments: {
+    REM This is a comment!
+    :: This is a comment, too!!
+  }
+  1.9.Silencing Display of Commands in Batch Files: {
+    - @ECHO OFF
+    - ECHO ON
+  }
+  1.10.Debugging Your Scripts: {
+    - No way, just print debugging messages via ECHO command.
+  }
+}
